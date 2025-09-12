b7b2ac0b1f4b9b1455266c6484253aefe1bcb661
digis <rodolfoh24@hotmail.com>
2025-09-12 13:54:30 -0600

    PRUEBA 7 - (Casi Listo)

--- src/main/java/com/RisosuIT/Pokedex/Controlador/ControladorFavoritos.java
+++ src/main/java/com/RisosuIT/Pokedex/Controlador/ControladorFavoritos.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/favoritos")
@@ -41,27 +43,33 @@ public class ControladorFavoritos {
     }
 
     // API JSON
-    @GetMapping("/json")
+    @GetMapping(value = "/json")
+    @ResponseBody
     public Set<Integer> obtenerFavoritos(HttpSession session) {
         Set<Integer> favs = (Set<Integer>) session.getAttribute("favoritos");
         return favs != null ? favs : Set.of();
     }
 
     @PostMapping("/add/{id}")
-    public void agregarFavorito(@PathVariable int id, HttpSession session) {
+    @ResponseBody
+    public ResponseEntity<Void> agregarFavorito(@PathVariable int id, HttpSession session) {
         Set<Integer> favs = (Set<Integer>) session.getAttribute("favoritos");
         if (favs == null) {
             favs = ConcurrentHashMap.newKeySet();
             session.setAttribute("favoritos", favs);
         }
         favs.add(id);
+        return ResponseEntity.ok().build(); // 200 sin vista
     }
 
     @PostMapping("/remove/{id}")
-    public void quitarFavorito(@PathVariable int id, HttpSession session) {
+    @ResponseBody
+    public ResponseEntity<Void> quitarFavorito(@PathVariable int id, HttpSession session) {
         Set<Integer> favs = (Set<Integer>) session.getAttribute("favoritos");
         if (favs != null) {
             favs.remove(id);
         }
+        return ResponseEntity.ok().build();
     }
+
 }
